e72ccfe5e0e8b9a23fab28fb54b49176b388fef8
DrGorilla_md <dr_gorilla@protonmail.com>
2023-01-12 23:10:55 +0100

    init

---
+++ test/JuiceDelegatesRegistry.t.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "@juice-delegate-registry/JuiceDelegatesRegistry.sol";
+
+contract JuiceDelegatesRegistryTest is Test {
+
+}
