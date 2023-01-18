e02134280c068e6b45bf7a56f4414df1725e5779
DrGorilla_md <dr_gorilla@protonmail.com>
2023-01-18 14:38:28 +0100

    create2 for future proofing

--- src/interfaces/IJBDelegatesRegistry.sol
+++ src/interfaces/IJBDelegatesRegistry.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.0;
 
 interface IJBDelegatesRegistry {
     function deployerOf(address _delegate) external view returns (address _deployer);
-    function addDelegate(address _delegate) external;
+    function addDelegate(address _deployer, uint256 _nonce) external;
+    function addDelegateCreate2(address _deployer, bytes32 _salt, bytes calldata _bytecode) external;
 }
