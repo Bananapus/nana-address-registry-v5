869427020277050134ad2924fe2e0e54c95baf7c
mejango <mejango@protonmail.com>
2024-01-08 21:46:13 -0500

    deploy script more explicit

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,8 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import "lib/forge-std/src/Script.sol";
+
 import "src/JBAddressRegistry.sol";
-import "forge-std/Script.sol";
 
 contract Deploy is Script {
     function run() public {
