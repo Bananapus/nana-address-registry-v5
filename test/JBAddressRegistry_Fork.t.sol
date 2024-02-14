2d3d4c2bc126088cfc26bd5a86679a3bf1e46739
Filip <f@filip.world>
2024-02-13 23:16:03 -0500

    many changes

--- test/JBAddressRegistry_Fork.t.sol
+++ test/JBAddressRegistry_Fork.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.23;
 
 import "forge-std/Test.sol";
-import "src/JBAddressRegistry.sol";
+import "../src/JBAddressRegistry.sol";
 
 contract JBAddressRegistryTest_Fork is Test {
     address owner = makeAddr("_owner");
