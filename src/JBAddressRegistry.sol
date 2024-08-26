71c6d72759cc56c85d5adcabe4c99a6b594c5f3e
mejango <mejango@protonmail.com>
2024-08-26 13:54:29 -0300

    order

--- src/JBAddressRegistry.sol
+++ src/JBAddressRegistry.sol
@@ -57,15 +57,6 @@ contract JBAddressRegistry is IJBAddressRegistry {
     // ---------------------- internal transactions ---------------------- //
     //*********************************************************************//
 
-    /// @notice Register a contract's deployer in the `deployerOf` mapping.
-    /// @param addr The deployed contract's address.
-    /// @param deployer The deployer's address.
-    function _registerAddress(address addr, address deployer) internal {
-        deployerOf[addr] = deployer;
-
-        emit AddressRegistered({addr: addr, deployer: deployer});
-    }
-
     /// @notice Compute the address of a contract deployed using `create` based on the deployer's address and nonce.
     /// @dev Taken from https://ethereum.stackexchange.com/a/87840/68134 - this won't work for nonces > 2**32. If
     /// you reach that nonce please: 1) ping us, because wow 2) use another deployer.
@@ -91,5 +82,14 @@ contract JBAddressRegistry is IJBAddressRegistry {
             mstore(0, hash)
             addr := mload(0)
         }
+    }
+
+    /// @notice Register a contract's deployer in the `deployerOf` mapping.
+    /// @param addr The deployed contract's address.
+    /// @param deployer The deployer's address.
+    function _registerAddress(address addr, address deployer) internal {
+        deployerOf[addr] = deployer;
+
+        emit AddressRegistered({addr: addr, deployer: deployer});
     }
 }
